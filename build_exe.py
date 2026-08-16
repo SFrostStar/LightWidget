3e817a6ad17650104c4c98e8374e1dc9d7f986d1
SFrostStar <dmitriyskin99@gmail.com>
2026-08-16 04:05:51 +0300

    Add automated GitHub Release workflow for macOS (.dmg) & Windows (.exe)

--- build_exe.py
+++ build_exe.py
@@ -1,4 +1,7 @@
 import sys
+import os
+import shutil
+import subprocess
 import PyInstaller.__main__
 
 def build():
@@ -7,7 +10,6 @@ def build():
     params = [
         'app.py',
         '--name=LightWidget',
-        '--onefile',
         '--windowed',
         '--noconfirm',
         '--clean',
@@ -15,14 +17,55 @@ def build():
         f'--add-data=ios{sep}ios',
         '--hidden-import=telethon',
         '--hidden-import=webview',
-        '--hidden-import=webview.platforms.winforms',
-        '--hidden-import=webview.platforms.edgechromium',
-        '--hidden-import=clr',
-        '--hidden-import=pythonnet',
         '--hidden-import=urllib.request',
     ]
     
+    if sys.platform == 'win32':
+        params.extend([
+            '--onefile',
+            '--hidden-import=webview.platforms.winforms',
+            '--hidden-import=webview.platforms.edgechromium',
+            '--hidden-import=clr',
+            '--hidden-import=pythonnet',
+        ])
+    else:
+        params.extend([
+            '--hidden-import=objc',
+            '--hidden-import=Cocoa',
+            '--hidden-import=Quartz',
+        ])
+    
     PyInstaller.__main__.run(params)
+
+    if sys.platform == 'darwin':
+        app_path = os.path.join("dist", "LightWidget.app")
+        if os.path.exists(app_path):
+            staging_dir = "dist_dmg"
+            if os.path.exists(staging_dir):
+                shutil.rmtree(staging_dir)
+            os.makedirs(staging_dir, exist_ok=True)
+            
+            shutil.copytree(app_path, os.path.join(staging_dir, "LightWidget.app"))
+            try:
+                os.symlink("/Applications", os.path.join(staging_dir, "Applications"))
+            except Exception:
+                pass
+
+            dmg_path = os.path.join("dist", "LightWidget-macOS.dmg")
+            if os.path.exists(dmg_path):
+                os.remove(dmg_path)
+
+            subprocess.run([
+                "hdiutil", "create",
+                "-volname", "LightWidget",
+                "-srcfolder", staging_dir,
+                "-ov",
+                "-format", "UDZO",
+                dmg_path
+            ], check=False)
+            
+            if os.path.exists(staging_dir):
+                shutil.rmtree(staging_dir)
 
 if __name__ == '__main__':
     build()
